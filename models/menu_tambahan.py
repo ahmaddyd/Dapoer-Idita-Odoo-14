cc86641d4d0e4c416e578b841ae1325c31b3d4b4
ahmaddyd <ahmdyulian.86@gmail.com>
2022-11-30 15:27:11 +0700

    Changes Description Models

--- models/menu_tambahan.py
+++ models/menu_tambahan.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models
 
 class MenuTambahan(models.Model):
     _name = 'dapoeridita.menu_tambahan'
-    _description = 'Deskripsi'
+    _description = 'Deskripsi Menu List Tambahan Dapoer Idita'
 
     name = fields.Char(string='Name')
     deskripsi = fields.Char(string='Deskripsi')
