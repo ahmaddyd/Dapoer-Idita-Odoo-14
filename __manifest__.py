f02ddd7f6cc50d5fa06f83fe69aa00f3d0f3dacc
ahmaddyd <ahmdyulian.86@gmail.com>
2022-11-30 14:27:04 +0700

    Add Views Menu Protein

--- __manifest__.py
+++ __manifest__.py
@@ -30,6 +30,7 @@
         'views/menu_views.xml',
         'views/menu_lauk_views.xml',
         'views/menu_minuman_views.xml',
+        'views/menu_protein_views.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
