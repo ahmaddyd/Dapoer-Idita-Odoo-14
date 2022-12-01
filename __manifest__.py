ad06052de6b2d168dc69d0148e7f04e999e8eb98
ahmaddyd <ahmdyulian.86@gmail.com>
2022-12-01 10:19:31 +0700

    Changes version and add views report

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Productivity',
-    'version': '1.2',
+    'version': '1.3',
     'application': True,
 
     # any module necessary for this one to work correctly
@@ -40,6 +40,8 @@
         'views/kemasan_box_views.xml',
         'views/pegawai_views.xml',
         'views/pelanggan_views.xml',
+        'report/report.xml',
+        'report/dapoeridita_invoice.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
